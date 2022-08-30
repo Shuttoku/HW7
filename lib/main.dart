53552b7b4075691fc0e60b8954c3b5ad14e1725f
Shuttoku <108716387+Shuttoku@users.noreply.github.com>
2022-08-30 14:29:46 +0700

    kjkj

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 
 void main() {
@@ -14,101 +13,96 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
 
-Widget myButton(String buttonLabel){
+String result = "0";
 
+
+  buttonPressed(String value) {
+    
+
+setState(() {
+  result = value;
+});
+    
+  }
+
+  Widget myButton(String buttonLabel) {
     return Expanded(
       child: OutlinedButton(
-     
-                      onPressed: () {},
-                      child: Text(
-                        buttonLabel,
-                        style: TextStyle(
-                          fontSize: 20,
-                        )
-                      ),
-                    ),
+        onPressed: () =>  buttonPressed(buttonLabel),
+        child: Text(buttonLabel,
+            style: TextStyle(
+              fontSize: 20,
+            )),
+      ),
     );
-
-
-}
-
-
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
           appBar: AppBar(
             title: Text("Calculator"),
           ),
           body: SafeArea(
             child: Column(
-            children: [
-              Container(
-                alignment: Alignment.centerRight,
-                child: Text(
-                  "Result",
-                  style: TextStyle(
-                    fontSize: 48,
-                    fontWeight: FontWeight.bold
+              children: [
+                Container(
+                  alignment: Alignment.centerRight,
+                  child: Text(
+                    result,
+                    style: TextStyle(fontSize: 48, fontWeight: FontWeight.bold),
                   ),
-
                 ),
-              ),
-              Expanded(
-                child: Divider(),
-              ),
-
-              Column(
-                children: [
-                  Row(
-                children: [
-                  myButton("7"),
-                  myButton("8"),
-                  myButton("9"),
-                  myButton("/"),
-                ],
-              ),
-
-              
-              Row(
-                children: [
-                  myButton("4"),
-                  myButton("5"),
-                  myButton("6"),
-                  myButton("x"),
-                ],
-              ),
-              Row(
-                children: [
-                  myButton("1"),
-                  myButton("2"),
-                  myButton("3"),
-                  myButton("-"),
-                ],
-              ),
-
-              Row(
-                children: [
-                  myButton("."),
-                  myButton("0"),
-                  myButton("00"),
-                  myButton("+"),
-                ],
-              ),
-              Row(
-                children: [
-                  myButton("CLEAR"),
-                  myButton("="),
-                ],
-              ),
-                ],
-              ),
-             ],    
+                Expanded(
+                  child: Divider(),
+                ),
+                Column(
+                  children: [
+                    Row(
+                      children: [
+                        myButton("7"),
+                        myButton("8"),
+                        myButton("9"),
+                        myButton("/"),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        myButton("4"),
+                        myButton("5"),
+                        myButton("6"),
+                        myButton("x"),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        myButton("1"),
+                        myButton("2"),
+                        myButton("3"),
+                        myButton("-"),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        myButton("."),
+                        myButton("0"),
+                        myButton("00"),
+                        myButton("+"),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        myButton("CLEAR"),
+                        myButton("="),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
-          ),
-    ));
-  
+        ));
   }
 }
